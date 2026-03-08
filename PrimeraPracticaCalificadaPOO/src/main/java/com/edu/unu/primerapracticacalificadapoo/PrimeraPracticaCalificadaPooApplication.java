7ae3387ef5a7cb7db96e401ce9537c48ada2bff5
fabrizzioabanto-dotcom <fabrizzioabanto@gmail.com>
2026-03-08 00:55:17 -0500

    Estructura del proyecto

---
+++ PrimeraPracticaCalificadaPOO/src/main/java/com/edu/unu/primerapracticacalificadapoo/PrimeraPracticaCalificadaPooApplication.java
@@ -0,0 +1,13 @@
+package com.edu.unu.primerapracticacalificadapoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraPracticaCalificadaPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeraPracticaCalificadaPooApplication.class, args);
+    }
+
+}
